c5a154ace62b8f30f2ed422c837a4cb218ca3cfa
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-03-20 14:51:00 +0530

    feat: added commander for custom cli

--- NOTES.md
+++ NOTES.md
@@ -2,3 +2,7 @@
 
 - With iterator function we can define our own iterations. Generator functions make it easy and yield holds value for a specific iteration
 - Here it is used to iterate over the stream(chunks) which will be received from the LLM.
+
+#### Error Handling
+
+- Exponential Backoff
--- main.ts
+++ main.ts
@@ -1,12 +1,18 @@
+import { program } from "commander";
 import { createLLMClient } from "./client/llm_client";
 
-async function main() {
+program.argument("<string>");
+program.parse();
+const prompt = program.args[0];
+console.log(program.args[0]);
+
+async function main(prompt: string | undefined) {
   const client = createLLMClient();
-  const messages = [{ role: "user", content: "What's up?" }];
+  const messages = [{ role: "user", content: prompt }];
 
   for await (const event of client.chatCompletion(messages, true)) {
     console.log(event);
   }
 }
 
-main();
+main(prompt);
